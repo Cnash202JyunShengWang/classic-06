ce53471080289cb2cf25133e02a479deccf73d3d
Roy <a0966750212@icloud.c0m>
2021-09-24 21:45:49 +0800

    Initial Commit

---
+++ classic 06/classic_06App.swift
@@ -0,0 +1,17 @@
+//
+//  classic_06App.swift
+//  classic 06
+//
+//  Created by 暨大附中 on 2021/9/24.
+//
+
+import SwiftUI
+
+@main
+struct classic_06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
